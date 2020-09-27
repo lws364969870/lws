65bc6ad2015969c2cdeeea7631d1d6ad02e2a923
lws364969870 <364969870@qq.com>
2020-09-28 01:13:57 +0800

    增加route3 demo

--- spring-cloud-family/spring-cloud-gateway-server/src/main/java/com/lws/gateway/route/GatewayRouteLocatorConfig.java
+++ spring-cloud-family/spring-cloud-gateway-server/src/main/java/com/lws/gateway/route/GatewayRouteLocatorConfig.java
@@ -20,12 +20,12 @@ public class GatewayRouteLocatorConfig {
     @Bean
     public RouteLocator customerRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(r -> r.path("/abc/**")
+                .route(r -> r.path("/route3/**")
                         .filters(f -> f.stripPrefix(1)
                                 .filter(elapsedGatewayFilter))
-                        .uri("lb://lws-demo-a")
+                        .uri("lb://lws-demo-b")
                         .order(0)
-                        .id("route-demo-3")
+                        .id("demo-a-route-3")
                 )
                 .build();
     }
