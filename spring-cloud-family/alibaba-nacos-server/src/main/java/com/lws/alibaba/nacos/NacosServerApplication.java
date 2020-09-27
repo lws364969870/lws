ee9ba7e68a23a185e7b90803741fa50d0e2f4d6d
lws364969870 <364969870@qq.com>
2020-09-28 02:35:23 +0800

    初始化nacos项目

---
+++ spring-cloud-family/alibaba-nacos-server/src/main/java/com/lws/alibaba/nacos/NacosServerApplication.java
@@ -0,0 +1,15 @@
+package com.lws.alibaba.nacos;
+
+
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@NacosPropertySource(dataId = "example", autoRefreshed = true)
+public class NacosServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosServerApplication.class, args);
+    }
+}
