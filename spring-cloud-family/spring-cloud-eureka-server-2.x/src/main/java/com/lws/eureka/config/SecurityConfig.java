44c67065974a2278f16b3b68039ee41d241aee1a
lws364969870 <364969870@qq.com>
2020-09-28 02:06:39 +0800

    帐号密码支持

---
+++ spring-cloud-family/spring-cloud-eureka-server-2.x/src/main/java/com/lws/eureka/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.lws.eureka.config;
+
+/**
+ * 新版Eureka启动帐号密码登录
+ */
+//@Configuration
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+//        super.configure(http);
+//    }
+//}
